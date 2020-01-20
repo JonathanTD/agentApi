8924f2db603bedc890f0b8f1e030a6853fb57bd1
Jonathan Tiede <jonathantiede@Jonathans-MacBook-Pro.local>
2020-01-20 11:11:31 -0700

    remove commented code

--- database/utils/createDb.js
+++ database/utils/createDb.js
@@ -1,6 +1,5 @@
 const agents = require('../seeders/agents');
 const customers = require('../seeders/customers');
-// const Agent = require('../src/api/models/Agent');
 const matchAndAdd = require('./joinCustomers');
 
 // create a client to mongodb
@@ -11,19 +10,6 @@ const matchedAgents = matchAndAdd(agents, customers);
 
 MongoClient.connect(url, function(err, db) {
   if (err) throw err;
-
-  // var dbo = db.db("agentapi");
-  // console.log(agents.length)
-  // agents.forEach(el => {
-  //   let person = new Agent(el);
-  //   // let person = new Agent({_id: el.id, ...el});
-  //   let addedCustomers = customers.filter(el => el.agent_id === person.id);
-  //   person.customers = [...addedCustomers];
-  //   dbo.collection("agents").insertOne(person, (err, res) => {
-  //     if(err) throw err;
-  //   })
-  // })
-    // })
   const dbo = db.db("agentapi");
 
   dbo.collection("agents").insertMany(matchedAgents, (err, res) => {
